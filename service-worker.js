b5193a1ea2c7120323f0eeb32e658aeed993f9e3
sksinghdeo <ssinghde@asu.edu>
2026-04-30 00:11:27 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "macro-logger-bc-v5-premium-icons";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./core_foods.json",
+  "./brand_foods.json",
+  "./restaurant_foods.json",
+  "./manifest.json",
+  "./icon.PNG",
+  "./logo_bc_v4.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
