ee5f50d445fd85e3175d138b4d87fcddde38e025
sksinghdeo <ssinghde@asu.edu>
2026-04-29 22:32:44 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "macro-logger-final-polish-mathpatch-v1";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./core_foods.json",
+  "./brand_foods.json",
+  "./restaurant_foods.json",
+  "./manifest.json",
+  "./icon.PNG"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
